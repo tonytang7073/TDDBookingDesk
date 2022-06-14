ed8f30d6483654ad75a9337735e27c23617d96ba
tonytang7073 <83436620+tonytang7073@users.noreply.github.com>
2022-06-14 14:05:10 +0800

    Add webtest project and change the dataaccess test to not rely on the seeddata.

--- BookingDesk.DataAccess.Test/Repositories/DeskRepoTest.cs
+++ BookingDesk.DataAccess.Test/Repositories/DeskRepoTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookingDesk.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookingDesk.DataAccess.Repositories
@@ -15,18 +16,24 @@ namespace BookingDesk.DataAccess.Repositories
             var options = new DbContextOptionsBuilder<BookingDeskDbContext>()
                 .UseInMemoryDatabase(databaseName: "ShouldReturnAll")
                 .Options;
-            
-            using(var ctx = new BookingDeskDbContext(options))
-            {
-                ctx.Database.EnsureCreated();
-            }
 
-            //act
+
+
 
             using (var ctx = new BookingDeskDbContext(options))
             {
+                ctx.Desk.AddRange(
+               new Desk { Id = new Guid("C0843157-B797-4C8D-B063-B55E53822F02"), Description = "Desk #1" },
+               new Desk { Id = new Guid("3B1CCD31-C286-45FD-AD73-5B2229916606"), Description = "Desk #2" },
+               new Desk { Id = new Guid("7BC3C939-FE90-47FB-A410-DAA8B3A95A8B"), Description = "Desk #11" }
+               );
+                ctx.SaveChanges();
+
+                //act
                 var repo = new DeskRepository(ctx);
                 var alldesks = repo.GetAllDesks().ToList();
+
+                //assert
                 Assert.Equal(3, alldesks.Count);
             }
 
@@ -44,9 +51,14 @@ namespace BookingDesk.DataAccess.Repositories
 
             using (var ctx = new BookingDeskDbContext(options))
             {
-                ctx.DeskBook.Add(new Domain.DeskBook { Date = date, DeskId= new Guid("C0843157-B797-4C8D-B063-B55E53822F02"), Id= Guid.NewGuid() });
-                ctx.DeskBook.Add(new Domain.DeskBook { Date = date.AddDays(1), DeskId = new Guid("C0843157-B797-4C8D-B063-B55E53822F02"), Id = Guid.NewGuid() });
-                ctx.Database.EnsureCreated();
+                ctx.Desk.AddRange(
+              new Desk { Id = new Guid("C0843157-B797-4C8D-B063-B55E53822F02"), Description = "Desk #1" },
+              new Desk { Id = new Guid("3B1CCD31-C286-45FD-AD73-5B2229916606"), Description = "Desk #2" },
+              new Desk { Id = new Guid("7BC3C939-FE90-47FB-A410-DAA8B3A95A8B"), Description = "Desk #11" }
+              );
+
+                ctx.DeskBook.Add(new Domain.DeskBook { Date = date, DeskId = new Guid("C0843157-B797-4C8D-B063-B55E53822F02"), Id = Guid.NewGuid(), Email="tt.tt.com", Firstname="tt", Lastname="ja" });
+                ctx.DeskBook.Add(new Domain.DeskBook { Date = date.AddDays(1), DeskId = new Guid("C0843157-B797-4C8D-B063-B55E53822F02"), Id = Guid.NewGuid(), Email = "tt.tt.com", Firstname = "tt", Lastname = "ja" });
                 ctx.SaveChanges();
             }
 
---
+++ BookingDesk.Web.Test/Pages/BookDeskModelTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingDesk.Domain;
+using BookingDesk.Processor;
+using BookingDesk.Web.Pages;
+using Moq;
+
+namespace BookingDesk.Web.Test.Pages
+{
+    public class BookDeskModelTests
+    {
+        private readonly Mock<IDeskBookingRequestProcess> _processMock;
+        private readonly BookDeskModel _bookDeskModel;
+        private readonly BookingResult _deskBookingResult;
+
+        public BookDeskModelTests()
+        {
+            _processMock = new Mock<IDeskBookingRequestProcess>();
+            _bookDeskModel = new BookDeskModel(_processMock.Object)
+            {
+                BookingRequest = new BookingRequest()
+            };
+
+            _deskBookingResult = new BookingResult()
+            {
+                ResultCode = BookingResultCode.Success
+            };
+
+            _processMock.Setup(x => x.BookDesk(_bookDeskModel.BookingRequest)).Returns(_deskBookingResult);
+
+
+        }
+
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(0, false)]
+        public void ShouldCallBookDeskMethodOfProcessIfModelIsValid(int expectedcalls, bool isModelValid)
+        {
+            if (!isModelValid)
+            {
+                _bookDeskModel.ModelState.AddModelError("AnyKey", "AnErrorMessage");
+            }
+
+            //act
+            _bookDeskModel.OnPost();
+
+            //assert
+            _processMock.Verify(x => x.BookDesk(_bookDeskModel.BookingRequest), Times.Exactly(expectedcalls));
+
+        }
+
+        [Fact]
+        public void ShouldAddModelErrorIfNoDeskIsAvailable()
+        {
+            //arrange
+            _deskBookingResult.ResultCode=BookingResultCode.NoDeskAvailable;
+
+            //act
+            _bookDeskModel.OnPost();
+
+            //assert
+            var modelState =
+            Assert.Contains("BookingRequest.Date", _bookDeskModel.ModelState);
+
+            var modelError = Assert.Single(modelState.Errors);
+            Assert.Equal("No desk available for selected date", modelError.ErrorMessage);
+
+        }
+
+
+
+    }
+}
